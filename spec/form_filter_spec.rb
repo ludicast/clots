f71670b3f75bb3256e1d44395ccb191714d96361
Nate Kidwell <nate@ludicast.com>
2009-06-30 00:57:58 -0400

    Cleaning tests

--- spec/form_filter_spec.rb
+++ spec/form_filter_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "Form Builder" do
+describe "Form Filter" do
 
   include Clot::UrlFilters
   include Clot::FormFilters
@@ -10,29 +10,14 @@ describe "Form Builder" do
     @context = {}    
   end
 
-
-  context "should set param on form items" do
-    it "that are closed" do
-      expected = '<input dummy="ffgg" ilse="sss"  type="password"/>'
-      template = '{{ \'<input dummy="ffgg" ilse="sss" />\' | set_param: "type", "password" }}'
-      template.should parse_to(expected)
-    end
-
-    it "that are open" do
-      expected = '<input dummy="ffgg" ilse="sss"  type="password">'
-      template = '{{ \'<input dummy="ffgg" ilse="sss" >\' | set_param: "type", "password" }}'
-      template.should parse_to(expected)
-    end
-  end
-
-  context "should translate to text area" do
-    it "converting vaues to text contents" do
+  context "input_to_text translates to text area" do
+    specify "converting value attribute to text contents" do
       expected = '<textarea>HELLO</textarea>'
       template = '{{ \'<input type="text" value="HELLO" />\' | input_to_text }}'
       template.should parse_to(expected)
     end
 
-    it "keeping original attributes" do
+    specify "keeping other attributes" do
       expected = '<textarea name="g-luv"></textarea>'
       template = '{{ \'<input type="text" name="g-luv" />\' | input_to_text }}'
       template.should parse_to(expected)
@@ -40,36 +25,51 @@ describe "Form Builder" do
 
   end
 
-  context "should label form items" do
-    it "should label form item" do
+  context "form_item filter" do
+    it "adds label to arbitrary data" do
       expected = "<p><label>hello there</label>form_item</p>"
       template = '{{"form_item" | form_item: "hello there" }}'
       template.should parse_to(expected)
     end
 
-    it "should have 'required' option for form item's label" do
+    it "includes 'required' class" do
       expected = '<p><label>hmm<span class="required">*</span></label>h2</p>'
       template = '{{"h2" | form_item: "hmm", true }}'
       template.should parse_to(expected)
     end
 
-    it "should populate 'for' attribute depending on 'id' field" do
+    it "populates 'for' attribute dwith contents of 'id' attribute" do
       expected = "<p><label for=\"item\">nyuk</label><i id=\"item\"></p>"
       template = '{{\'<i id="item">\' | form_item: "nyuk" }}'
       template.should parse_to(expected)
     end
   end
+ 
+  context "the set_param filter" do
+    it "should let you change params on an input" do
+      expected = '<input dummy="ffgg" type="password" ilse="sss" />'
+      template = '{{ \'<input dummy="ffgg" type="text" ilse="sss" />\' | set_param: "type", "password" }}'
+      template.should parse_to(expected)
+    end
 
-  it "should let you set params on an input" do
-    expected = '<input dummy="ffgg" type="password" ilse="sss" />'
-    template = '{{ \'<input dummy="ffgg" type="text" ilse="sss" />\' | set_param: "type", "password" }}'
-    template.should parse_to(expected)
+    it "should let you add params to an input" do
+      expected = '<input dummy="ffgg" ilse="sss"  type="password"/>'
+      template = '{{ \'<input dummy="ffgg" ilse="sss" />\' | set_param: "type", "password" }}'
+      template.should parse_to(expected)
+    end
+
+    it "should work on tags that are open" do
+      expected = '<input dummy="ffgg" ilse="sss"  type="password">'
+      template = '{{ \'<input dummy="ffgg" ilse="sss" >\' | set_param: "type", "password" }}'
+      template.should parse_to(expected)
+    end      
   end
-
-  it "should produce parsed version of template" do
-    expected = '<div class="form-submit-button"><input type="submit" value="I am here"/></div>'
-    template = '{{ "I am here" | submit_button }}'
-    template.should parse_to(expected)
-  end  
-
+  
+  context "the submit_button filter" do
+    it "converts text to submit button" do
+      expected = '<div class="form-submit-button"><input type="submit" value="I am here"/></div>'
+      template = '{{ "I am here" | submit_button }}'
+      template.should parse_to(expected)
+    end
+  end
 end
--- spec/url_filter_spec.rb
+++ spec/url_filter_spec.rb
@@ -10,39 +10,39 @@ describe "Url Filters" do
     @context = {}    
   end
 
-
-  context "for stylesheets" do
-    it "should get url" do
+  context "stylesheet_url filter" do
+    specify "should get url for stylesheet" do
       test_url = stylesheet_url "stylesheet.css"
       test_url.should == "/stylesheets/stylesheet.css"
+    end 
+  end
+
+  context "object_url filter" do
+    specify "should get url for object" do
+      obj = get_drop @@text_content_default_values
+      test_url = object_url obj
+      test_url.should == "/" + obj.dropped_class.to_s.tableize + "/" + obj.record_id.to_s
+    end
+
+    specify "with label should get url for object within alternate class" do
+      obj = get_drop @@text_content_default_values
+      test_url = object_url obj, "image_contents"
+      test_url.should == "/image_contents/" + obj.record_id.to_s
     end
   end
 
- 
-  it "should get url for object" do
-    obj = get_drop @@text_content_default_values
-    test_url = object_url obj
-    test_url.should == "/" + obj.dropped_class.to_s.tableize + "/" + obj.record_id.to_s
-  end
-
-  it "should get url for object within alternate class" do  
-    obj = get_drop @@text_content_default_values
-    test_url = object_url obj, "image_contents"
-    test_url.should == "/image_contents/" + obj.record_id.to_s
-  end
-
-  context "it should produce nested urls" do
+  context "get_nested_url filter should produce nested urls" do
     before(:each) do
       @obj = get_drop @@text_content_default_values
     end
 
-    it "should have default nestings" do
+    specify "of objects" do
       url = get_nested_url @obj, @obj
       expected_url = "/liquid_demo_models/1/liquid_demo_models/1"
       url.should == expected_url
     end
 
-    it "should allow you to nested resource" do
+    specify "of classes" do
       url = get_nested_url @obj, "/child"
       expected_url = "/liquid_demo_models/1/child"
       url.should == expected_url
