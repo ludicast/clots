310a937ad4635f5d97baa99760b84036eac7c875
Nate Kidwell <nate@ludicast.com>
2010-03-15 14:34:50 -0400

    added prompt for date_select

--- spec/model_date_tag_spec.rb
+++ spec/model_date_tag_spec.rb
@@ -65,6 +65,19 @@ describe "tags for forms that use models" do
 
       tag_should_parse_to @year_string + @month_string + @day_string
     end
+
+
+    it "should take prompts" do
+      @tag = "{% date_select dummy,'registered_at',day_prompt:'Choose Day',month_prompt:'Choose Month',year_prompt:'Choose Year' %}"
+
+      @year_string = '<select id="dummy_registered_at_1i" name="dummy[registered_at(1i)]"><option value="">Choose Year</option>' + get_options(@time.year-5,@time.year - 1) + %{<option selected="selected" value="#{@time.year}">#{@time.year}</option>} + get_options(@time.year + 1,@time.year + 5) + "</select>"
+      @month_string = '<select id="dummy_registered_at_2i" name="dummy[registered_at(2i)]"><option value="">Choose Month</option>' + get_options(1,@time.month - 1,{:label_func => :get_month}) + %{<option selected="selected" value="#{@time.month}">#{get_month @time.month}</option>} + get_options(@time.month + 1,12,{:label_func => :get_month}) + "</select>"
+      @day_string = '<select id="dummy_registered_at_3i" name="dummy[registered_at(3i)]"><option value="">Choose Day</option>' + get_options(1,(@time.day - 1)) + %{<option selected="selected" value="#{@time.day}">#{fill_zeros @time.day}</option>} + get_options((@time.day + 1),31) + "</select>"
+
+
+      tag_should_parse_to @year_string + @month_string + @day_string
+    end
+
     it "should generate selection with start year" do
       @tag = "{% date_select dummy,'registered_at',start_year:1995 %}"
 
@@ -157,6 +170,7 @@ describe "tags for forms that use models" do
             
       tag_should_parse_to @year_string + @month_string + @day_string + @hour_string + @minute_string
     end
+
 
   end
 end
